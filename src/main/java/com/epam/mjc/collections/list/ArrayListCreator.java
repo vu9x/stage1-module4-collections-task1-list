b250419af412ef6b8bb83a220fac8003b5cd71cf
Vu <nguyentuanvu9x@gmail.com>
2023-12-20 21:34:50 +0300

    Finished: ArrayListCreator

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -1,9 +1,22 @@
 package com.epam.mjc.collections.list;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String> destinationList = new ArrayList<>();
+
+        for (int i = 0; i < sourceList.size(); i++) {
+            if((i+1) % 3 == 0){
+                Collections.addAll(destinationList, sourceList.get(i), sourceList.get(i));
+            }
+        }
+
+        return destinationList;
     }
 }
+
+
